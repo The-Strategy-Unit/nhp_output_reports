2e15106683d9a41e43288f7710cc492252a13f54
Jacqueline <jacqueline.grout1@nhs.net>
2026-03-13 18:55:13 +0000

    Sort and order table

--- R/get_tpma_impact_table.R
+++ R/get_tpma_impact_table.R
@@ -11,13 +11,45 @@ mits <- read_mitigators()
 
 impact <- get_impact_data(soc_scenario,obc_scenario, site_codes)
 
-tbl_tpma <- dplyr::left_join(impact, tpma, by = dplyr::join_by(activity_type, strategy)) |>
+tpma <- dplyr::left_join(impact, tpma, by = dplyr::join_by(activity_type, strategy)) |>
   dplyr::select(c(strategy, measure, range_soc, impact_soc, range_obc, impact_obc)) |>
   dplyr::left_join(mits) |>
   dplyr::select(-strategy) |>
   dplyr::relocate(mitigator_name) |>
-  dplyr::filter(!is.na(mitigator_name)) |>
+  dplyr::filter(!is.na(mitigator_name))  |>
+  dplyr::arrange(measure, mitigator_name)    # new line
 
+
+# This section is to force the range onto the arrivals impact for these mitigators
+sdec_veryhigh <-tpma |>
+  dplyr::filter(mitigator_name=="Same Day Emergency Care (Very High Potential)" & measure == "beddays") |>
+  dplyr::pull(range_obc)
+
+sdec_high <- tpma |>
+  dplyr::filter(mitigator_name=="Same Day Emergency Care (High Potential)" & measure == "beddays") |>
+  dplyr::pull(range_obc)
+
+sdec_moderate <- tpma |>
+  dplyr::filter(mitigator_name=="Same Day Emergency Care (Moderate Potential)" & measure == "beddays") |>
+  dplyr::pull(range_obc)
+
+sdec_low <- tpma |>
+  dplyr::filter(mitigator_name=="Same Day Emergency Care (Low Potential)" & measure == "beddays") |>
+  dplyr::pull(range_obc)
+
+
+tpma <- tpma |>
+  dplyr::mutate(range_obc = dplyr::case_when(mitigator_name=="Same Day Emergency Care (Very High Potential)" & measure != "beddays" ~ sdec_veryhigh,
+                                             mitigator_name=="Same Day Emergency Care (High Potential)" & measure != "beddays" ~ sdec_high,
+                                             mitigator_name=="Same Day Emergency Care (Moderate Potential)" & measure != "beddays" ~ sdec_moderate,
+                                             mitigator_name=="Same Day Emergency Care (Low Potential)" & measure != "beddays" ~ sdec_low,
+                                             .default = range_obc)
+  )
+
+# build table, rows by A&E, IP, OP.
+# order of sub-rows not as per the Word doc, can manually enter row-names in order if necessary
+
+tbl_tpma <- tpma |>
   gt::gt(rowname_col = "mitigator_name") |>
   gt::tab_stubhead(label = "TPMA") |>
   gt::fmt_number(
@@ -26,6 +58,20 @@ tbl_tpma <- dplyr::left_join(impact, tpma, by = dplyr::join_by(activity_type, st
 
   gt::sub_missing(
     missing_text = "-"
+  ) |>
+  gt::tab_row_group(
+    label = "Outpatients",
+    rows = measure %in% c("attendances", "tele_attendances")
+  ) |>
+
+  gt::tab_row_group(
+    label = "Inpatients",
+    rows = measure == "beddays"
+  ) |>
+
+  gt::tab_row_group(
+    label = "A&E",
+    rows = measure == "arrivals"
   ) |>
 
   gt::cols_label(
