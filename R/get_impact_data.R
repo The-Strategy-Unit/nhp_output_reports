e906afa8f5e4062ec1e57ce751449c4088e0bccc
Jacqueline Grout <103451105+jacgrout@users.noreply.github.com>
2026-03-17 18:07:22 +0000

    Update R/get_impact_data.R
    Co-authored-by: Matt Dray <18232097+matt-dray@users.noreply.github.com>

--- R/get_impact_data.R
+++ R/get_impact_data.R
@@ -4,9 +4,10 @@ get_impact_data <- function(soc_scenario, obc_scenario, site_codes){
   impact_soc <- get_stepcounts(soc_scenario) |>
     dplyr::filter(change_factor %in% c("activity_avoidance", "efficiencies")) |>
     filter_sites_conditionally(site_codes$ip) |>
-    dplyr::group_by(strategy, activity_type, measure) |>
-    dplyr::summarise(impact_soc = sum(value)) |>
-    dplyr::ungroup()
+    dplyr::summarise(
+      impact_soc = sum(value),
+      .by = c(strategy, activity_type, measure)
+    ) |>
 
   impact_obc <- get_stepcounts(obc_scenario) |>
     dplyr::filter(change_factor == "activity_avoidance" | change_factor == "efficiencies") |>
